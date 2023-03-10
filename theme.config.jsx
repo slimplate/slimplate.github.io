9be740b281f838df947da8d4d081ffeaf1825d68
David Konsumer <konsumer@jetboystudio.com>
2023-03-10 11:56:57 -0800

    mionor config and add slimplate json

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
 
-const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+const config = {
+  logo: <span>Slimplate</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/slimplate/slimplate'
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/slimplate/slimplate.github.io/tree/main/',
   footer: {
-    text: 'Nextra Docs Template',
-  },
+    text: 'slimplate'
+  }
 }
 
 export default config
