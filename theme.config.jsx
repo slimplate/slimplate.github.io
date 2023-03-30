02749f203014bb9b9b1a8b4b10b966d3c8d9a330
David Konsumer <konsumer@jetboystudio.com>
2023-03-30 12:31:15 -0700

    minor tweaks

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config = {
   logo: <span>Slimplate</span>,
@@ -8,6 +10,24 @@ const config = {
   docsRepositoryBase: 'https://github.com/slimplate/slimplate.github.io/tree/main/',
   footer: {
     text: 'slimplate'
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://slimplate.github.io' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+    const title = frontMatter.title ? `${frontMatter.title} - Slimplate` : 'Slimplate'
+
+    return (
+      <>
+        <meta property='og:url' content={url} />
+        <meta property='og:title' content={title} />
+        <meta property='og:description' content={frontMatter.description || 'A light and easy CMS for any site'} />
+        <title>{title}</title>
+      </>
+    )
   }
 }
 
